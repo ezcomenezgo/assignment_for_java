0710e2819e6ea62203963e65105afe8f0c2ebf4a
Claire Li <s3310507057@gmail.com>
2023-12-11 15:25:03 +0000

    project init

---
+++ src/main/java/com/example/wholesale_system/WholesaleSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.wholesale_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WholesaleSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WholesaleSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wholesale_system/WholesaleSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wholesale_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WholesaleSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
